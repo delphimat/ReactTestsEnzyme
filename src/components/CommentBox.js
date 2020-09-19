aa544d53cab6a8fbaf06bde0192e77d11bab0132
Mathieu Delphin <delphi.mat@gmail.com>
2020-09-19 21:32:31 +0200

    work on High Order Component

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -1,10 +1,13 @@
 import React, {Component} from "react";
 import { connect } from 'react-redux';
 import * as actions from 'actions';
+import requireAuth from "./requireAuth";
 
 class CommentBox extends  Component {
 
     state = { comment: ''};
+
+
 
     handleChange = (event) => {
         this.setState({comment: event.target.value})
@@ -36,4 +39,6 @@ class CommentBox extends  Component {
     }
 }
 
-export default connect(null, actions)(CommentBox);
+
+
+export default connect(null, actions)(requireAuth(CommentBox));
---
+++ src/components/requireAuth.js
@@ -0,0 +1,36 @@
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+
+export default (ChildComponent) => {
+
+    class ComposedComponent extends Component {
+
+        // our component just go rendered
+        componentDidMount() {
+            this.shouldNavigateAway();
+        }
+
+        // our component just got updated
+        componentDidUpdate() {
+            this.shouldNavigateAway();
+        }
+
+        shouldNavigateAway() {
+            if (!this.props.auth) {
+                console.log('I NEED TO LEAVE!!!');
+                this.props.history.push('/');
+            }
+        }
+
+        render() {
+            return <ChildComponent {...this.props} />;
+        }
+    }
+
+
+    function mapStateToProps(state) {
+        return { auth: state.auth};
+    }
+
+    return connect(mapStateToProps)(ComposedComponent);
+};
